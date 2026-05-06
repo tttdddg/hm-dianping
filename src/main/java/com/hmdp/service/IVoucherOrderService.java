9505f4f32e0b5189f12203b7c9035fddb6f5a9ce
tttdddg <2091445482@qq.com>
2026-05-06 20:24:37 +0800

    库存超卖问题

--- src/main/java/com/hmdp/service/IVoucherOrderService.java
+++ src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -8,11 +8,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * <p>
  *  服务类
  * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
  */
 public interface IVoucherOrderService extends IService<VoucherOrder> {
 
     Result seckillVoucher(Long voucherId);
+
+    Result createVoucherOrder(Long voucherId);
 }
--- src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
@@ -77,9 +77,12 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
 
         //扣减库存
         boolean success=seckillVoucherService.update()
-                .setSql("stock=stock-1")
-                .eq("voucher_id",voucherId).update();
+                .setSql("stock=stock-1") //set stock=stock-1
+//                .eq("voucher_id",voucherId).eq("stock",voucher.getStock()) //where id=? and stock=?
+                .eq("voucher_id",voucherId).gt("stock",0) ////where id=? and stock>0
+                .update();
         if(!success){
+            //扣减失败
             return Result.fail("库存不足");
         }
 
